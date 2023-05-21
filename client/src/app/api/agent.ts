9d8994ef140bc552ee6331d93b28e839374b34a0
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-05-21 19:42:00 +0300

    tostify for errors added

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,4 +1,6 @@
+import { TonalitySharp } from "@mui/icons-material";
 import axios, { AxiosError, AxiosResponse } from "axios";
+import {toast} from "react-toastify";
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
@@ -7,8 +9,24 @@ const responseBody = (response: AxiosResponse) => response.data;
 axios.interceptors.response.use(response => {
     return response
 }, (error: AxiosError)=>{
-    console.log('caught by interceptor')
-    return Promise.reject(error.response)
+    const {data, status} = error.response as AxiosResponse;
+    switch(status){
+        case 400:
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 404:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+        default:
+            break;
+    }
+    return Promise.reject(error.response);
 })
 
 const requests = {
