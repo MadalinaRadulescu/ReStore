f8f6d1298786b639c2bdd107d0f17e731ab9b48f
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-05-22 15:42:19 +0300

    basket controller + dtos for basket

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { router } from "../router/Routes";
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL = 'http://localhost:5001/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
 
