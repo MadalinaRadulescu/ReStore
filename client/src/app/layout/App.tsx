392f292a356646c52f9da663885c492e4466d9c9
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-05-24 11:14:32 +0300

    item count cart icon updated

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,33 @@
 import { Container, createTheme, CssBaseline, Switch, ThemeProvider } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Header from "./Header";
 import "react-toastify/dist/ReactToastify.css"
 import HomePage from "../../features/home/HomePage";
 import Catalog from "../../features/catalog/Catalog";
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if(buyerId){
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(()=> setLoading(false));
+    } else{
+      setLoading(false);
+    }
+  }, [setBasket]);
+
+
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark':'light';
   const theme = createTheme({
@@ -22,6 +42,8 @@ function App() {
   function handelThemeChange(){
     setDarkMode(!darkMode);
   }
+
+  if(loading) return <LoadingComponent message="Initialising app..."/>
   
   return (
     <ThemeProvider theme={theme}>
