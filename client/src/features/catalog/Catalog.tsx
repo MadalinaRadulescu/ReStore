225169403b52d4bd0772e9a7c8b4fdb6b1c36082
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-06-08 10:43:17 +0300

    pagination done

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,9 +1,11 @@
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup, Typography } from "@mui/material";
+import { Box, Grid, Pagination, Paper, Typography } from "@mui/material";
 import { useEffect } from "react";
+import AppPagination from "../../app/components/AppPagination";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
 import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchFiltersAsync, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice";
+import { fetchFiltersAsync, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
 import ProductSearch from "./ProductSearch";
 
@@ -16,7 +18,7 @@ const sortOptions = [
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded, brands, types, productParams} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded, brands, types, productParams, metaData} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
   useEffect(()=>{
@@ -27,10 +29,10 @@ export default function Catalog() {
     if(!filtersLoaded) dispatch(fetchFiltersAsync());
   }, [dispatch, filtersLoaded]);
 
-  if(status.includes('pending')) return <LoadingComponent message = 'Loading products...'/>
+  if(status.includes('pending') || !metaData) return <LoadingComponent message = 'Loading products...'/>
 
     return(
-        <Grid container spacing={4}>
+        <Grid container columnSpacing={4}>
           <Grid item xs={3}>
             <Paper sx={{mb: 2}}>
               <ProductSearch />
@@ -43,18 +45,19 @@ export default function Catalog() {
               />
             </Paper>
             <Paper sx={{mb: 2, p: 2}}>
-              <FormGroup>
-                {brands.map(brand => (
-                  <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
-                ))}
-              </FormGroup>
+              <CheckboxButtons 
+                items={brands}
+                checked={productParams.brands}
+                onChange={(items: string[]) => dispatch(setProductParams({brands: items}))}
+              />
+
             </Paper>
             <Paper sx={{mb: 2, p: 2}}>
-              <FormGroup>
-                {types.map(type => (
-                  <FormControlLabel control={<Checkbox />} label={type} key={type} />
-                ))}
-              </FormGroup>
+            <CheckboxButtons 
+                items={types}
+                checked={productParams.types}
+                onChange={(items: string[]) => dispatch(setProductParams({types: items}))}
+              />
             </Paper>
 
           </Grid>
@@ -62,18 +65,11 @@ export default function Catalog() {
             <ProductList products={products}/>
           </Grid>
           <Grid item xs={3}/>
-          <Grid item xs={9}> 
-            <Box display = 'flex' justifyContent='space-between' alignItems='center'>
-              <Typography>
-                Displaing 1-6 of 18 items
-              </Typography>
-              <Pagination 
-                color='secondary'
-                size='large'
-                count={10}
-                page={2}
-              />
-            </Box>
+          <Grid item xs={9} sx={{mb: 2}}> 
+            <AppPagination 
+              metaData={metaData}
+              onPageChange= {(page: number) => dispatch(setPageNumber({pageNumber: page}))}
+            />
           </Grid>
         </Grid>
         
