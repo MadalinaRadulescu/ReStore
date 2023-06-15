c5b697c065f2db1ed6f4ba011acad98d7ea7487f
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-06-15 13:54:10 +0300

    account slice added

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { accountSlice } from "../../features/account/accountSlice";
 import { basketSlice } from "../../features/basket/basketSlice";
 import { catalogSlice } from "../../features/catalog/catalogSlice";
 
@@ -7,7 +8,8 @@ import { catalogSlice } from "../../features/catalog/catalogSlice";
 export const store = configureStore({
     reducer: {
         basket: basketSlice.reducer,
-        catalog: catalogSlice.reducer
+        catalog: catalogSlice.reducer,
+        account: accountSlice.reducer
     }
 })
 
