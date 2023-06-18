1b6df56398fd54a2afb82ba7f11e387c3efdcb76
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-06-18 12:39:19 +0300

    persisting login

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import {toast} from "react-toastify";
+import { getConfigFileParsingDiagnostics } from "typescript";
 import { PaginatedResponse } from "../models/pagination";
 import { router } from "../router/Routes";
+import { store } from "../store/configureStore";
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
 
@@ -9,6 +11,12 @@ axios.defaults.baseURL = 'http://localhost:5001/api/';
 axios.defaults.withCredentials = true;
 
 const responseBody = (response: AxiosResponse) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().account.user?.token;
+    if(token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
     await sleep();
