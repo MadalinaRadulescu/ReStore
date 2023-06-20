45feacbe6cdddcea54d43b5106ca5d59ff912360
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-06-20 09:55:12 +0300

    reusable checkbox + validation library

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,9 +1,14 @@
-import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Header from "./Header";
-import "react-toastify/dist/ReactToastify.css"
+import "react-toastify/dist/ReactToastify.css";
 import LoadingComponent from "./LoadingComponent";
 import { useAppDispatch } from "../store/configureStore";
 import { fetchBasketAsync } from "../../features/basket/basketSlice";
@@ -12,38 +17,37 @@ import { fetchCurrentUser } from "../../features/account/accountSlice";
 function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
-  
-  const  initApp = useCallback(async () =>  {
+
+  const initApp = useCallback(async () => {
     try {
       await dispatch(fetchCurrentUser());
       await dispatch(fetchBasketAsync());
     } catch (error: any) {
-      console.log(error)
+      console.log(error);
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
     initApp().then(() => setLoading(false));
   }, [initApp]);
 
-
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark':'light';
+  const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea' : '#121212'
-      }
-    }
-  })
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
 
-  function handelThemeChange(){
+  function handelThemeChange() {
     setDarkMode(!darkMode);
   }
 
-  if(loading) return <LoadingComponent message="Initialising app..."/>
-  
+  if (loading) return <LoadingComponent message="Initialising app..." />;
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -14,29 +14,25 @@ import Register from "../../features/account/Register";
 import RequireAuth from "./RequireAuth";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {element: <RequireAuth />, children: [
-                {path: 'checkout', element: <CheckoutPage/>},
-            ]},
-            {path: '', element: <HomePage />},
-            {path: 'catalog', element: <Catalog />},
-            {path: 'catalog/:id', element: <ProductDetails />},
-            {path: 'about', element: <AboutPage />},
-            {path: 'contact', element: <ContactPage />},
-            {path: 'server-error', element: <ServerError />},
-            {path: 'not-found', element: <NotFound/>},
-            {path: 'basket', element: <BasketPage/>},
-            
-            {path: 'login', element: <Login/>},
-            {path: 'register', element: <Register/>},
-            {path: '*', element: <Navigate replace to='/not-found'/>},
-            
-
-
-
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <RequireAuth />,
+        children: [{ path: "checkout", element: <CheckoutPage /> }],
+      },
+      { path: "", element: <HomePage /> },
+      { path: "catalog", element: <Catalog /> },
+      { path: "catalog/:id", element: <ProductDetails /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "contact", element: <ContactPage /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "basket", element: <BasketPage /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "*", element: <Navigate replace to="/not-found" /> },
+    ],
+  },
+]);
