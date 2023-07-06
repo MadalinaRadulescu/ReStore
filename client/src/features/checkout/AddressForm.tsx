ee91a11ba95e29dd8c34f16c70e27fcf3b45e00b
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2023-07-06 12:51:24 +0300

    orders page created

--- client/src/features/checkout/AddressForm.tsx
+++ client/src/features/checkout/AddressForm.tsx
@@ -7,7 +7,7 @@ import AppCheckbox from "../../app/components/AppCheckbox";
 import AppTextInput from "../../app/components/AppTextInput";
 
 export default function AddressForm() {
-  const { control } = useFormContext();
+  const { control, formState } = useFormContext();
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -41,7 +41,12 @@ export default function AddressForm() {
           <AppTextInput control={control} name="country" label="Country" />
         </Grid>
         <Grid item xs={12}>
-          <AppCheckbox name='saveAddress' label='Save this as the default address' control={control} />
+          <AppCheckbox 
+            disabled={!formState.isDirty}
+            name='saveAddress' 
+            label='Save this as the default address' 
+            control={control} 
+            />
         </Grid>
       </Grid>
     </>
